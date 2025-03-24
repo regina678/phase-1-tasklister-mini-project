57875922f1385ee3b46455c8850d7780dd0df9a7
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-03-24 11:56:28 +0300

    simple to do list that allow users to add and delete items

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault(); 
+  
+      const taskDescription = document.getElementById("new-task-description").value;
+  
+  
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskDescription;
+      taskItem.style.color = "black"; 
+  
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "❌";
+      deleteButton.style.marginLeft = "10px";
+      deleteButton.addEventListener("click", () => {
+        taskItem.remove();
+      });
+  
+  
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+  
+      form.reset();
+    });
+  
 });
